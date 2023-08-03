da19acc629c0c0e1ca1130b29fda0970b622686c
PedroHenriquevr <pedrohvribeiro@gmail.com>
2023-08-03 09:02:59 -0300

    create project expo

---
+++ src/pages/Carrinho/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Carrinho() {
+    return(
+        <View>
+            <Text>Carrinho de compras</Text>
+        </View>
+    )
+}
